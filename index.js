86fdb471d7d77d8ca7b47700eeed9b3498009ed3
Andre <andre.axel@outlook.com>
2022-07-05 12:24:55 -0300

    V1 - Anexado Texto e Separaçao em Div

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-var listaFilmes =
+var filmsPlot =
 ["https://cineclick-static.flixmedia.cloud/1280/615461cf-b76a-4952-a8b1-cddaaf35706f.jpg",
- "https://s2.glbimg.com/fCNS_fXPhGRwlpnAaQLzvGYC1y0=/362x536/https://s2.glbimg.com/UPI9xlM9R9O41YRqSO7R3hTNk_s=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/q/V/Avo9ISQQO3grnARi3JCA/2020-914-a-chegada-poster.jpg",
- "https://i.ytimg.com/vi/uaSYEUugnzE/movieposter_en.jpg",
- "https://img.travessa.com.br/DVD/GR/44/443cb71d-0799-466f-9c9d-ac3bd21715fd.jpg"]
+ "https://br.web.img3.acsta.net/pictures/17/04/19/21/08/577190.jpg",
+ "https://br.web.img3.acsta.net/medias/nmedia/18/90/95/96/20122166.jpg",
+ "https://images-na.ssl-images-amazon.com/images/I/81+RNiMeExL.jpg",
+ "https://m.media-amazon.com/images/I/511+gBWbA-L._AC_SY445_.jpg",
+ "https://m.media-amazon.com/images/I/51B9vDgCmoL._AC_.jpg"]
 
-for(var i=0; i < listaFilmes.length; i++){
-    document.write("<img class='image' src=" + listaFilmes[i] + ">")
+for(var i=0; i < filmsPlot.length; i++){
+    document.write("<img class='image' src=" + filmsPlot[i] + ">")
 }
-/*
-function adicionarFilme(){
-    var filmeFavorito = document.getElementById("filme").value;
-    if(filmeFavorito.endsWith(".jpg")){
-        listarFilmesNaTela(filmeFavorito)
-    }else{
-        console.error("Adicione um endereço válido")
-    }
-    /*Para deixar o input vazio depois de adicionar
-    document.getElementById("filme").value = "";
-}*/
 
 function listarFilmesNaTela(filme){
     console.log(filme)
