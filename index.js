d574dfa04033dd95a8c9a7eb33db3e08422522ed
Andre <andre.axel@outlook.com>
2022-07-07 12:56:28 -0300

    V1 - Scroll right just Mobile

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ var filmsPlot =
  "https://br.web.img3.acsta.net/medias/nmedia/18/90/95/96/20122166.jpg",
  "https://images-na.ssl-images-amazon.com/images/I/81+RNiMeExL.jpg",
  "https://m.media-amazon.com/images/I/511+gBWbA-L._AC_SY445_.jpg",
- "https://m.media-amazon.com/images/I/51B9vDgCmoL._AC_.jpg"]
+ "https://m.media-amazon.com/images/I/51B9vDgCmoL._AC_.jpg",
+ "https://m.media-amazon.com/images/I/81io9SYH5dL._AC_SL1500_.jpg",
+ "http://www.cinevitor.com.br/wp-content/uploads/2015/10/oilusionistaposter.jpg"]
 
 for(var i=0; i < filmsPlot.length; i++){
-    document.write("<img class='image' src=" + filmsPlot[i] + ">")
+    document.write("<img class='image-dois' src=" + filmsPlot[i] + ">")
 }
 
-function listarFilmesNaTela(filme){
+function listFilms(filme){
     console.log(filme)
     var elementoFilmeFavorito = "<img src=" + filme + ">";
-    var elementoListaFilmes = document.getElementById("listaFilmes");
+    var elementoListaFilmes = document.getElementById("listFilms");
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
 }
+
+document.addEventListener("touchstart", function() {}, true);
