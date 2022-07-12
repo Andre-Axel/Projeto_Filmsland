60e9f296f66b519a4bb6f7bdf8b5ddff25601e7f
Andre <andre.axel@outlook.com>
2022-07-11 22:10:00 -0300

    V1 - Adicionado mais filmes

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var filmsPlot =
  "https://images-na.ssl-images-amazon.com/images/I/81+RNiMeExL.jpg",
  "https://m.media-amazon.com/images/I/511+gBWbA-L._AC_SY445_.jpg",
  "https://m.media-amazon.com/images/I/51B9vDgCmoL._AC_.jpg",
+ "https://upload.wikimedia.org/wikipedia/pt/thumb/d/d2/Valilla%2Bsky%2Bposter.jpg/255px-Valilla%2Bsky%2Bposter.jpg",
+ "https://br.web.img3.acsta.net/pictures/19/02/07/14/16/5034340.jpg",
+ "https://media.fstatic.com/Gf5GKUqVUNaDwmGm5-nXp0L6Z9g=/290x478/smart/media/movies/covers/2011/11/d7dd71941870ff014758322db8c4f469.jpg",
+ "https://br.web.img3.acsta.net/pictures/210/124/21012465_2013061319170245.jpg",
+ "https://br.web.img3.acsta.net/c_310_420/pictures/16/10/19/01/57/552532.jpg",
  "https://m.media-amazon.com/images/I/81io9SYH5dL._AC_SL1500_.jpg",
  "http://www.cinevitor.com.br/wp-content/uploads/2015/10/oilusionistaposter.jpg"]
 
--- traveltime.js
+++ traveltime.js
@@ -1,8 +1,16 @@
 var filmsTravelTime =
-["https://cineclick-static.flixmedia.cloud/1280/615461cf-b76a-4952-a8b1-cddaaf35706f.jpg",
- "https://br.web.img3.acsta.net/pictures/17/04/19/21/08/577190.jpg",
+[
  "https://m.media-amazon.com/images/I/511+gBWbA-L._AC_SY445_.jpg",
- "https://m.media-amazon.com/images/I/51B9vDgCmoL._AC_.jpg"]
+ "https://br.web.img2.acsta.net/pictures/22/03/02/17/11/3732338.png",
+ "https://br.web.img3.acsta.net/pictures/21/05/26/20/19/2966501.jpg",
+ "https://media.fstatic.com/z8hEgE7vww4MtD47QaDPqsYvvIA=/290x478/smart/media/movies/covers/2018/01/Projeto_Almanaque.jpg",
+ "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/71/20267083.jpg",
+ "https://br.web.img3.acsta.net/pictures/13/12/18/20/56/273919.jpg",
+ "https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/86/99/50/19870912.jpg",
+ "https://br.web.img3.acsta.net/pictures/14/05/27/15/43/549754.jpg",
+ "https://br.web.img3.acsta.net/medias/nmedia/18/92/25/86/20188919.jpg",
+ "https://cineclick-static.flixmedia.cloud/1280/615461cf-b76a-4952-a8b1-cddaaf35706f.jpg",
+]
 
 for(var i=0; i < filmsTravelTime.length; i++){
     document.write("<img class='image-dois' src=" + filmsTravelTime[i] + ">")
